07179ffc4779b632550f2843c018414a895b5a86
globalqss <devnull@localhost>
2008-01-04 01:11:44 +0000

    Oracle migration script for LCO

---
+++ migration/01_alter_add_reteiva.sql
@@ -0,0 +1,10 @@
+ALTER TABLE adempiereqss.LCO_WITHHOLDINGRULECONF
+ ADD (isuseproducttaxcategory  CHAR(1)
+DEFAULT 'N' NOT NULL CHECK (isuseproducttaxcategory IN ('Y',
+   'N')));
+
+ALTER TABLE LCO_WITHHOLDINGRULE ADD (c_taxcategory_id  NUMBER(10));
+
+ALTER TABLE LCO_WITHHOLDINGRULE ADD   CONSTRAINT taxcategory_withholding
+ FOREIGN KEY (c_taxcategory_id)
+ REFERENCES C_TAXCATEGORY (c_taxcategory_id);
